b4236f3222a02e22a05e4b87c6e63585847d6101
Pooja Singh <poojasingh42001@gmail.com>
2023-05-27 18:12:54 +0530

    project structure 1

---
+++ src/main/java/com/example/liveasy/logistics/LiveasyLogistics1Application.java
@@ -0,0 +1,13 @@
+package com.example.liveasy.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveasyLogistics1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveasyLogistics1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liveasy/logistics/LiveasyLogistics1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liveasy.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveasyLogistics1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
